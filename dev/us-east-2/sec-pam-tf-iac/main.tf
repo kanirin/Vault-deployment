a8501ac41bb53ca7edfb4de910f04e545cdb83a8
kanirin <krinatova95@gmail.com>
2023-06-23 09:58:34 -0500

    provider

--- dev/us-east-2/sec-pam-tf-iac/main.tf
+++ dev/us-east-2/sec-pam-tf-iac/main.tf
@@ -4,6 +4,16 @@ resource "aws_default_vpc" "default" {
   }
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+  # Configuration options
 }
